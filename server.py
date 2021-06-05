192a2396e4bdf2598585b0057bd06db7a494f634
Huy <hmitran@ucdavis.edu>
2021-06-05 14:21:54 -0700

    final

--- server.py
+++ server.py
@@ -4,29 +4,80 @@ import os
 
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
-def model(in_arr):
-    colNames = list(range(1024))
-    colNames.append("label")
-    df = pd.read_csv('./qsar_oral_toxicity.csv', sep=';',names=colNames)
-    df = pd.get_dummies(df)
-    df.drop("label_negative",1,inplace=True)
-    df = df.rename({"label_positive":"label"},axis=1)
-    X_train = df.drop(["label"],1)
-    y_train = df["label"]
-    X_train.reset_index(drop=True, inplace=True)
+def del_index_list(list_object, indices):
+    indices = sorted(indices, reverse=True)
+    for i in indices:
+        if i < len(list_object):
+            #print("before:", list_object)
+            list_object.pop(i)
+            #print("after:", list_object)
 
-    val = pd.Series(in_arr)
-    classifier = RandomForestClassifier(n_estimators=500, max_depth=70)
-    classifier.fit(X_train, y_train)
-    res = classifier.predict([val])[0]
-    if res == 1:
-        label = "VERY TOXIC"
-    else:
-        label = "NOT VERY TOXIC"
-    return label
+#classifier = 0
+#df = 0
+#df_shape = 0#comparable with 0
+#to_drop_returned = 0
+
+def init_train_data():#failed optimization
+	colNames = list(range(1024))
+	colNames.append("label")
+	df = pd.read_csv('./qsar_oral_toxicity.csv', sep=';',names=colNames)
+	df = pd.get_dummies(df)
+	df.drop("label_negative",1,inplace=True)
+	df = df.rename({"label_positive":"label"},axis=1)
+
+	# Create correlation matrix
+	corr_matrix = df.corr().abs()
+	# Select upper triangle of correlation matrix (not including diagonal)
+	upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+	# Find features with correlation greater than 0.95
+	to_drop = [column for column in upper.columns if any(upper[column] > 0.95)]
+	# Drop features 
+	df.drop(to_drop, axis=1, inplace=True)
+
+	df_shape = df.shape
+	print("here")
+	return to_drop
+
+def model(in_arr):
+	threshold = 0.28
+	#optimize perf
+
+	colNames = list(range(1024))
+	colNames.append("label")
+	df = pd.read_csv('./qsar_oral_toxicity.csv', sep=';',names=colNames)
+	df = pd.get_dummies(df)
+	df.drop("label_negative",1,inplace=True)
+	df = df.rename({"label_positive":"label"},axis=1)
+	#print(3)
+	#print(type(df))
+	# Create correlation matrix
+	corr_matrix = df.corr().abs()
+	# Select upper triangle of correlation matrix (not including diagonal)
+	upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+	# Find features with correlation greater than 0.95
+	to_drop = [column for column in upper.columns if any(upper[column] > 0.95)]
+	# Drop features 
+	df.drop(to_drop, axis=1, inplace=True)
+
+	del_index_list(in_arr, to_drop)
+
+	X_train = df.drop(["label"],1)
+	y_train = df["label"]
+	X_train.reset_index(drop=True, inplace=True)
+
+	val = pd.Series(in_arr)
+	classifier = RandomForestClassifier(criterion="entropy", max_depth=63, max_features="auto", n_estimators=603)
+	classifier.fit(X_train, y_train)
+	res = classifier.predict_proba([val])[:,1][0]
+	if res >= threshold:
+	    label = "VERY TOXIC"
+	else:
+	    label = "NOT VERY TOXIC"
+	return label
 
 #Upper just capitalizes the message
 def checkValid(encoding):
